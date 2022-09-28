3d24977ec424b43b7575448dec442e1b43627ea2
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-09-28 14:38:29 -0300

    adicionando a funcao de abrir mais produtos na pagina

--- js/script.js
+++ js/script.js
@@ -5,26 +5,26 @@ async function showProducts() {
   let htmlProducts;  
 
 
-  function response(dados){
-    return dados.json()
+  function response(response){
+    return response.json()
   }
 
-  function dadosApi(response){
+  function dadosApi(dados){
     setTimeout(function(){
       spinner.style.display = 'none'
 
-      for(let i = 0; i <= response.products.length; i++){
+      for(let i = 0; i <= dados.products.length; i++){
         htmlProducts = `
         <div class="product-single">
-        <img class="image" src = "${response.products[i].image}"></img>
+        <img class="image" src = "${dados.products[i].image}"></img>
         <div class="info-price">
-          <h3>${response.products[i].name}</h3>
+          <h3>${dados.products[i].name}</h3>
           <p>
-            ${response.products[i].description}
+            ${dados.products[i].description}
           </p>
-          <span class="in">De: R$${response.products[i].oldPrice},00</span>
-          <span class="per">Por: R$${response.products[i].price},00</span>
-          <span class="split-price">ou ${response.products[i].installments.count}x de R$${response.products[i].installments.value}</span>
+          <span class="in">De: R$${dados.products[i].oldPrice},00</span>
+          <span class="per">Por: R$${dados.products[i].price},00</span>
+          <span class="split-price">ou ${dados.products[i].installments.count}x de R$${dados.products[i].installments.value}</span>
           <button>Comprar</button>
         </div>
       </div>
@@ -32,11 +32,59 @@ async function showProducts() {
       productsWrapper.innerHTML += htmlProducts
       }
     }, 800)
-
   }
 
   await fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1').then(response).then(dadosApi)
 
 }
 
+function showMoreProducts(){
+  const btnProductsMore = document.querySelector('.more-products')
+  const moreProducts = document.querySelector('.more-products-api')
+  const spinner = document.querySelector('.spinner-more')
+
+  let htmlProducts;
+  btnProductsMore.addEventListener('click', async function(){
+
+    spinner.style.display = 'block'
+
+    function response(response){
+      return response.json()
+    }
+
+    function dadosApi(dados){
+
+      setTimeout(function(){
+        spinner.style.display = 'none'
+
+        for(let i = 0; i <= dados.products.length; i++){
+          htmlProducts = `
+          <div class="product-single">
+            <img class="image" src = "${dados.products[i].image}"></img>
+            <div class="info-price">
+              <h3>${dados.products[i].name}</h3>
+              <p>
+                ${dados.products[i].description}
+              </p>
+              <span class="in">De: R$${dados.products[i].oldPrice},00</span>
+              <span class="per">Por: R$${dados.products[i].price},00</span>
+              <span class="split-price">ou ${dados.products[i].installments.count}x de R$${dados.products[i].installments.value}</span>
+              <button>Comprar</button>
+            </div>
+          </div>
+          `
+          
+          btnProductsMore.style.display = 'none'
+          moreProducts.innerHTML += htmlProducts
+        }
+
+      }, 800)
+    }
+
+    await fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2').then(response).then(dadosApi)
+
+  })
+}
+
 showProducts()
+showMoreProducts()
