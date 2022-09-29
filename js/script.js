92cc01969c776dc404c7e2dd43b89edc200b7cf7
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-09-29 11:34:11 -0300

    adicionando a funcao que verifica os campos do formulario de registro

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ function showMoreProducts(){
     setTimeout(function(){
         spinner.style.display = 'none'
 
-        for(let i = 0; i <= dados.products.length; i++){
+        for(let i = 0; i < dados.products.length; i++){
           htmlProducts = `
           <div class="product-single">
             <img class="image" src = "${dados.products[i].image}"></img>
@@ -89,5 +89,81 @@ function showMoreProducts(){
   })
 }
 
+function verifyForm(){
+  let form = document.querySelector('#registration-user')
+  let verifyErrorForm = false;
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const inputName = document.forms['registration-user']['input-name']
+    const regexName = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/
+    const verifyName = regexName.test(inputName.value)
+
+    if(!verifyName){
+      verifyErrorForm = true
+      inputName.classList.add('error-input')
+      let span = inputName.nextElementSibling
+      span.innerHTML = 'Insira um nome válido'
+    }else{
+      inputName.classList.remove('error-input')
+      let span = inputName.nextElementSibling
+      span.innerHTML = ''
+    }
+    
+    const inputEmail = document.forms['registration-user']['input-email']
+    const regexEmail = /\S+@\S+\.\S+/
+    const verifyEmail = regexEmail.test(inputEmail.value)
+
+    if(!verifyEmail){
+      verifyErrorForm = true
+      inputEmail.classList.add('error-input')
+      let span = inputEmail.nextElementSibling
+      span.innerHTML = 'Insira um e-mail válido'
+    }else{
+      inputEmail.classList.remove('error-input')
+      let span = inputEmail.nextElementSibling
+      span.innerHTML = ''
+    }
+
+
+    const inputCPF = document.forms['registration-user']['input-cpf']
+    const regexCPF = /^([\d]{3})([\d]{3})([\d]{3})([\d]{2})$/
+    const verifyCPF = regexCPF.test(inputCPF.value)
+
+    if(!verifyCPF){
+      verifyErrorForm = true
+      inputCPF.classList.add('error-input')
+      let span = inputCPF.nextElementSibling
+      span.innerHTML = 'Insira um cpf válido'
+    }else{
+      inputCPF.classList.remove('error-input')
+      let span = inputCPF.nextElementSibling
+      span.innerHTML = ''
+    }
+
+    const inputSex = document.forms['registration-user']['sex']
+    
+    if(!inputSex.value){
+      verifyErrorForm = true
+      const labels = document.querySelectorAll('.check-sex label')
+      labels.forEach((label) => {
+      label.style.color = 'red'
+      })
+    }else{
+      const labels = document.querySelectorAll('.check-sex label')
+      labels.forEach((label) => {
+      label.style.color = 'var(--color-primary)'
+      })
+    }
+
+    if(!verifyErrorForm){
+      form.submit()
+    }
+
+  })
+}
+
 showProducts()
 showMoreProducts()
+verifyForm()
